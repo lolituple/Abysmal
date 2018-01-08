eab4cacbfcefdfd05964e5f2e666dc3bf1ffb01a
lolituple <34293633+lolituple@users.noreply.github.com>
2018-01-08 20:18:49 +0800

    x

--- Abysmal-master/Abysmal/envs/Abysmal_env.py
+++ Abysmal-master/Abysmal/envs/Abysmal_env.py
@@ -158,7 +158,7 @@ class AbysmalEnv(gym.Env):
         self.num2_pic=random.randint(0,Num_num-1)
     def Ran(self):
         #return random.randint(1,9)
-        return 1    
+        return 9    
     def _render(self, mode='human', close=False):
         '''
         state=np.zeros((28,56))
